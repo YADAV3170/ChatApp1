f0379454af33dc3ef17679c86f7430889b5832ba
unknown <ssy382415@gmail.com>
2024-12-29 12:07:03 +0530

    Project Commited

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -4,6 +4,12 @@ import { config } from "dotenv";
 
 config();
 
+cloudinary.image("vieste_italy.jpg", {transformation: [
+  {width: 1000, crop: "scale"},
+  {quality: "auto"},
+  {fetch_format: "auto"}
+  ]});
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
